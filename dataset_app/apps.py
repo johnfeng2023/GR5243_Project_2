bb76bf35dd10818b8e996f7ff8d72ffa66651a8c
johnfeng2023 <148817624+johnfeng2023@users.noreply.github.com>
2025-03-07 14:06:41 -0500

    Delete dataset_app directory

--- dataset_app/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class DatasetAppConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "dataset_app"
--- dataset_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import home
-
-urlpatterns = [
-    path("", home, name="home"),  # This will load index.html at the home page
-]
-
